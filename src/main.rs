2af64f9c6bc05c5145bdaec63d97d7b7da70d6b8
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-28 15:30:19 +0100

    add snake head to grid

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ const CELL_COLOR: Color = Color::srgb(0.85, 0.85, 0.85);
 const CELL_BORDER_THICKNESS: f32 = 2.0;
 
 const BALL_COLOR: Color = Color::srgb(0.3, 0.3, 0.7);
-const BALL_SIZE: f32 = 50.0;
-const BALL_SPEED: f32 = 400.0;
 
 const CELL_SIZE: f32 = 50.0;
 const GRID_SIZE: Vec2 = Vec2::new(10.0, 10.0);
@@ -30,6 +28,7 @@ fn setup(
 ) {
     commands.spawn(Camera2d);
 
+    // grid cells
     for row in 0..GRID_SIZE.y as i32 {
         for col in 0..GRID_SIZE.x as i32 {
             commands.spawn((
@@ -42,7 +41,8 @@ fn setup(
                     translation: Vec3::new(
                         col as f32 * CELL_SIZE - (GRID_SIZE.x * CELL_SIZE) / 2.0 + CELL_SIZE / 2.0,
                         row as f32 * CELL_SIZE - (GRID_SIZE.y * CELL_SIZE) / 2.0 + CELL_SIZE / 2.0,
-                        0.0,
+                        // in background
+                        -10.0,
                     ),
                     ..default()
                 },
@@ -50,14 +50,16 @@ fn setup(
         }
     }
 
-    // let shape = meshes.add(Circle::new(BALL_SIZE));
-
-    // commands.spawn((
-    //     Mesh2d(shape),
-    //     MeshMaterial2d(materials.add(BALL_COLOR)),
-    //     Transform::default(),
-    //     SnakeHead,
-    // ));
+    let shape = meshes.add(Circle::new((CELL_SIZE / 2.0) * 0.8));
+    commands.spawn((
+        Mesh2d(shape),
+        MeshMaterial2d(materials.add(BALL_COLOR)),
+        Transform {
+            translation: grid_pos_to_world_pos(Vec2::new(GRID_SIZE.x / 2.0, GRID_SIZE.y / 2.0)),
+            ..default()
+        },
+        SnakeHead,
+    ));
 }
 
 // fn move_ball(
@@ -89,3 +91,18 @@ fn setup(
 
 //     ball_transform.translation = new_ball_position;
 // }
+
+fn grid_pos_to_world_pos(grid_pos: Vec2) -> Vec3 {
+    Vec3::new(
+        grid_pos.x * CELL_SIZE - (GRID_SIZE.x * CELL_SIZE) / 2.0 + CELL_SIZE / 2.0,
+        grid_pos.y * CELL_SIZE - (GRID_SIZE.y * CELL_SIZE) / 2.0 + CELL_SIZE / 2.0,
+        0.0,
+    )
+}
+
+fn world_pos_to_grid_pos(world_pos: Vec3) -> Vec2 {
+    Vec2::new(
+        ((world_pos.x + (GRID_SIZE.x * CELL_SIZE) / 2.0) / CELL_SIZE).floor(),
+        ((world_pos.y + (GRID_SIZE.y * CELL_SIZE) / 2.0) / CELL_SIZE).floor(),
+    )
+}
