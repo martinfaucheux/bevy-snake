2a525e3869a4a5e464447125262b6468cc89584c
Martin Faucheux <martin.faucheux@gmail.com>
2025-12-01 16:02:14 +0100

    spawn apple at random pos

--- src/plugins/apple/mod.rs
+++ src/plugins/apple/mod.rs
@@ -1,6 +1,8 @@
 use crate::core::*;
 use bevy::prelude::*;
 
+use rand::Rng;
+
 pub struct ApplePlugin;
 
 impl Plugin for ApplePlugin {
@@ -13,18 +15,36 @@ fn create_apple(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    grid_position_query: Query<&GridPosition>,
 ) {
+    let mut apple_position = get_random_grid_pos();
+    loop {
+        if !grid_position_query
+            .iter()
+            .any(|pos| pos.0 == apple_position)
+        {
+            break;
+        }
+        apple_position = get_random_grid_pos();
+    }
+
     let shape = meshes.add(Circle::new(CELL_SIZE * 0.3));
     let material = MeshMaterial2d(materials.add(APPLE_COLOR));
-    let apple_grid_pos = IVec2::new(1, 1);
     commands.spawn((
         Mesh2d(shape),
         material,
         Transform {
-            translation: grid_pos_to_world_pos(apple_grid_pos),
+            translation: grid_pos_to_world_pos(apple_position),
             ..default()
         },
         Apple,
-        GridPosition(apple_grid_pos),
+        GridPosition(apple_position),
     ));
+}
+
+fn get_random_grid_pos() -> IVec2 {
+    IVec2::new(
+        rand::rng().random_range(0..GRID_SIZE.x) as i32,
+        rand::rng().random_range(0..GRID_SIZE.y) as i32,
+    )
 }
