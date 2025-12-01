6d2ffcbfde67f5c540664852a4e455e14f6743eb
Martin Faucheux <martin.faucheux@gmail.com>
2025-12-01 16:06:52 +0100

    export get_random_unoccupied_grid_pos func to helpers

--- src/core/helpers.rs
+++ src/core/helpers.rs
@@ -1,5 +1,7 @@
+use super::components::GridPosition;
 use super::constants::*;
 use bevy::prelude::*;
+use rand::Rng;
 
 pub fn grid_pos_to_world_pos(grid_pos: IVec2) -> Vec3 {
     Vec3::new(
@@ -14,4 +16,20 @@ pub fn world_pos_to_grid_pos(world_pos: Vec3) -> IVec2 {
         (((world_pos.x + (GRID_SIZE.x as f32 * CELL_SIZE) / 2.0) / CELL_SIZE).floor()) as i32,
         (((world_pos.y + (GRID_SIZE.y as f32 * CELL_SIZE) / 2.0) / CELL_SIZE).floor()) as i32,
     )
+}
+
+pub fn get_random_grid_pos() -> IVec2 {
+    IVec2::new(
+        rand::rng().random_range(0..GRID_SIZE.x) as i32,
+        rand::rng().random_range(0..GRID_SIZE.y) as i32,
+    )
+}
+
+/// Gets a random grid position that is not occupied by any entity with a GridPosition component
+pub fn get_random_unoccupied_grid_pos(grid_position_query: &Query<&GridPosition>) -> IVec2 {
+    let mut position = get_random_grid_pos();
+    while grid_position_query.iter().any(|pos| pos.0 == position) {
+        position = get_random_grid_pos();
+    }
+    position
 }
--- src/plugins/apple/mod.rs
+++ src/plugins/apple/mod.rs
@@ -1,8 +1,6 @@
 use crate::core::*;
 use bevy::prelude::*;
 
-use rand::Rng;
-
 pub struct ApplePlugin;
 
 impl Plugin for ApplePlugin {
@@ -17,16 +15,7 @@ fn create_apple(
     mut materials: ResMut<Assets<ColorMaterial>>,
     grid_position_query: Query<&GridPosition>,
 ) {
-    let mut apple_position = get_random_grid_pos();
-    loop {
-        if !grid_position_query
-            .iter()
-            .any(|pos| pos.0 == apple_position)
-        {
-            break;
-        }
-        apple_position = get_random_grid_pos();
-    }
+    let apple_position = get_random_unoccupied_grid_pos(&grid_position_query);
 
     let shape = meshes.add(Circle::new(CELL_SIZE * 0.3));
     let material = MeshMaterial2d(materials.add(APPLE_COLOR));
@@ -40,11 +29,4 @@ fn create_apple(
         Apple,
         GridPosition(apple_position),
     ));
-}
-
-fn get_random_grid_pos() -> IVec2 {
-    IVec2::new(
-        rand::rng().random_range(0..GRID_SIZE.x) as i32,
-        rand::rng().random_range(0..GRID_SIZE.y) as i32,
-    )
 }
